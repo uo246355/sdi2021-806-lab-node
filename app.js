76a69bbf7821dca8ae3d85a76c9c2ff418534e1a
uo246355 <uo246355@uniovi.es>
2021-03-23 13:23:06 +0100

    SDI-2021-806-7.5-Vistas y motores de plantillas

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 
+let swig = require('swig');
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,9 +11,9 @@ app.use(express.static('public'));
 // Variables
 app.set('port', 8081);
 
-//Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app); // (app, param1, param2, etc.)
+///Rutas/controladores por lógica
+require("./routes/rusuarios.js")(app, swig);
+require("./routes/rcanciones.js")(app, swig);
 
 // lanzar el servidor
 app.listen(app.get('port'), function () {
