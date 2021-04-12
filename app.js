46e19da00ae84f3003ec7ece221fe36cc0d7c82b
uo246355 <uo246355@uniovi.es>
2021-04-12 18:47:42 +0200

    SDI-806-8.1-Acceso a datos de Mongo desde Node.js

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-let express = require('express');
-let app = express();
+//mongodb://atlas:sdi@tiendamusica-shard-00-00.qmudf.mongodb.net:27017,tiendamusica-shard-00-01.qmudf.mongodb.net:27017,tiendamusica-shard-00-02.qmudf.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-7fytht-shard-0&authSource=admin&retryWrites=true&w=majority
 
+
+
+let mongo = require('mongodb');
 let swig = require('swig');
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
-
 // Variables
 app.set('port', 8081);
+app.set('db',' mongodb://admin:<password>@tiendamusica-shard-00-00-hy8gh.mongodb.net:27017,mongodb://atlas:sdi@tiendamusica-shard-00-00.qmudf.mongodb.net:27017,tiendamusica-shard-00-01.qmudf.mongodb.net:27017,tiendamusica-shard-00-02.qmudf.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-7fytht-shard-0&authSource=admin&retryWrites=true&w=majority');
+
 
 ///Rutas/controladores por lógica
 require("./routes/rusuarios.js")(app, swig);
-require("./routes/rcanciones.js")(app, swig);
+require("./routes/rcanciones.js")(app, swig, mongo);
 
 // lanzar el servidor
 app.listen(app.get('port'), function () {
--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,25 +1,27 @@
-module.exports = function(app, swig) {
+module.exports = function(app, swig, mongo) {
     app.get("/canciones", function(req, res) {
-        let canciones = [{
-            "nombre" : "Blank space",
-            "precio" : "1.2"
-        },
-            {
-                "nombre" : "See you again",
-                "precio" : "1.3"
-            },
-            {
-                "nombre" : "Uptown Funk",
-                "precio" : "1.1"
+        let cancion = {
+            nombre : req.body.nombre,
+            genero : req.body.genero,
+            precio : req.body.precio
+        }
+
+        // Conectarse
+        mongo.MongoClient.connect(app.get('db'), function(err, db) {
+            if (err) {
+                res.send("Error de conexión: " + err);
+            } else {
+                let collection = db.collection('canciones');
+                collection.insertOne(cancion, function(err, result) {
+                    if (err) {
+                        res.send("Error al insertar " + err);
+                    } else {
+                        res.send("Agregada id: "+ result.ops[0]._id);
+                    }
+                    db.close();
+                });
             }
-        ];
-
-        let respuesta = swig.renderFile('views/btienda.html', {
-            vendedor : 'Tienda de canciones',
-            canciones : canciones
         });
-
-        res.send(respuesta);
     });
 
     app.get('/canciones/agregar', function (req, res) {
